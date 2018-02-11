ab55ac63ce0189d6e820a2a4b9b1f57503aabd2d
JJ Wyatt <jon-joshua@live.com>
2018-02-11 23:15:35 +0000

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from decoder import MidiDecoder
-
-# file = open('runescape-sea shanty 2.mid', 'rb')
-file = open('test.mid', 'rb')
-midi = MidiDecoder().decode(file)
-
-for x in midi.tracks[1].events:
-	# x = midi.tracks[1].events[y]
-	print('{: <5} {: <5} {: <6} {}    {}'.format(x.delta, hex(x.event), x.channel, x.message, x.get_name()))
